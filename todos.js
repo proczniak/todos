0a83443db83a76821096bc008ee30e72e31dedbf
Piotr Roczniak <proczniak@gmail.com>
2016-02-23 19:22:18 +0100

    just initialized

---
+++ todos.js
@@ -0,0 +1,89 @@
+Todos = new Mongo.Collection('todos');
+
+
+if(Meteor.isServer){
+  // server code goes here
+
+}
+
+if(Meteor.isClient){
+  // client code goes here
+
+  Template.todos.helpers({
+    'todo': function(){
+      return Todos.find({}, {sort: {createdAt: -1}});
+    }
+
+  });
+
+  Template.todoItem.helpers({
+    'checked': function(){
+      var isCompleted = this.completed;
+      if(isCompleted){
+        return "checked";
+      } else {
+        return "";
+      }
+    }
+  });
+
+  Template.todosCount.helpers({
+    'totalTodos': function(){
+      return Todos.find().count();
+    },
+    'completedTodos': function(){
+      return Todos.find({ completed: true }).count();
+      }
+  });
+
+  Template.addTodo.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var todoNAme = $('[name="todoName"]').val();
+      Todos.insert({
+        name: todoNAme,
+        completed: false,
+        createdAt: new Date()
+      });
+      $('[name="todoName"]').val('');
+    }
+  });
+
+  Template.todoItem.events({
+    'click .delete-todo': function(event){
+      event.preventDefault();
+      var documentId = this._id;
+
+      var confirm = window.confirm("Seriously?");
+      if(confirm) {
+        Todos.remove({_id: documentId});
+      }
+    },
+    'keyup [name=todoItem]': function(event){
+      if(event.which == 13 || event.which == 27) {
+        $(event.target).blur();
+      } else {
+        var documentId = this._id;
+        var todoItem = $(event.target).val();
+        Todos.update({_id: documentId}, {$set: {name: todoItem}});
+      }
+    },
+    'change [type=checkbox]': function(){
+      var documentId = this._id;
+      var isCompleted = this.completed;
+      if(isCompleted){
+        Todos.update({ _id: documentId }, {$set: { completed: false}});
+        console.log("marked incomplete");
+      } else {
+        Todos.update({ _id: documentId }, {$set: { completed: true}});
+        console.log("marked complete");
+      }
+    }
+  });
+}
+
+Router.route('/', {
+  template: 'home'
+});
+Router.route('/register');
+Router.route('/login');
