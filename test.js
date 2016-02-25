3cc121ad2baeff3924e70ecdb5717c2b898eec29
Piotr Roczniak <proczniak@gmail.com>
2016-02-26 00:35:31 +0100

    validation

---
+++ test.js
@@ -0,0 +1,14 @@
+//pierwsza bardzo długa linia
+//pierwsza bardzo długa linia
+//pierwsza bardzo długa linia
+//pierwsza bardzo długa linia
+//pierwsza bardzo długa linia
+
+
+var x = function(event){
+  var y = function(event){
+    var z = function(event){
+      var a = "Dupa";
+    }
+  }
+}
