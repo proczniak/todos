030e36096b3c769b53f046c016f7f427543d7ee5
Piotr Roczniak <proczniak@gmail.com>
2016-02-25 19:34:58 +0100

    router complexities even more

--- todos.js
+++ todos.js
@@ -149,7 +149,10 @@ if(Meteor.isClient){
           console.log(error.reason);
           Router.go('login');
         } else {
-          Router.go('home');
+          var currentRoute = Router.current().route.getName();
+          if (currentRoute == "login") {
+            Router.go('home');
+          }
         }
       });
 
